2358ace415fdbded99fd1d51a2d222d57a90b0d1
Hashim <hashimmufti1999@gmail.com>
2021-09-08 03:05:19 +1000

    Adding docustrings + code quality cleanup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random as random
+import random as rand
 import copy
 import os
 import sys
@@ -6,6 +6,14 @@ import getopt
 
 
 def game_of_life(some_board):
+    """Play a single iteration of Conway's Game of Life on a board.
+
+    Args:
+        some_board (List of lists of strings): List of lists containing the ALIVE/DEAD variable.
+
+    Returns:
+        [List of lists of strings]: List of lists containing the updated ALIVE/DEAD variables.
+    """
     r_index = -1
     duplicate_board = copy.deepcopy(some_board)
     for rows in some_board:
@@ -22,6 +30,18 @@ def game_of_life(some_board):
 
 
 def rules(r_index, c_index, lives, some_board, duplicate_board):
+    """Apply Conway's Rules to a board
+
+    Args:
+        r_index (int): Current row index
+        c_index (int): Current column index
+        lives (int): Number of ALIVE cells around current position
+        some_board (List of lists of strings): Board used to determine rule
+        duplicate_board (List of lists of strings): Board used to apply rule
+
+    Returns:
+        [List of lists of strings]: Board used to apply rule (modified board)
+    """
     if some_board[r_index][c_index] == ALIVE:
         if lives < 2 or lives > 3:
             duplicate_board[r_index][c_index] = DEAD
@@ -32,15 +52,35 @@ def rules(r_index, c_index, lives, some_board, duplicate_board):
 
 
 def get_lives(neighbors, some_board, rows, columns):
+    """Get all the ALIVE cells around current position
+
+    Args:
+        neighbors (List of integers): List of row and column of neighbor
+        some_board (List of lists of strings): Board used to find neighbors
+        rows (int): Current row
+        columns (int)): Current column
+
+    Returns:
+        [int]: Number of alive cells around current position
+    """
     alive = 0
     for neighbor in neighbors:
-        if valid(neighbor, rows, columns):
-            if some_board[neighbor[0]][neighbor[1]] == ALIVE:
-                alive += 1
+        if valid(neighbor, rows, columns) and some_board[neighbor[0]][neighbor[1]] == ALIVE:
+            alive += 1
     return alive
 
 
 def valid(neighbor, rows, columns):
+    """Find out if neighbor cell is valid
+
+    Args:
+        neighbor (List of integers): Neighboring cell position
+        rows (int): Number of rows on the board
+        columns (int): Number of columns on the board
+
+    Returns:
+        [boolean]: True if valid, False otherwise
+    """
     if neighbor[0] < 0 or neighbor[1] < 0:
         return False
     if neighbor[0] >= rows or neighbor[1] >= columns:
@@ -49,6 +89,15 @@ def valid(neighbor, rows, columns):
 
 
 def get_neighbors(r_index, c_index):
+    """Get neighboring cell positions
+
+    Args:
+        r_index (int): Current row index
+        c_index ([type]): Current column index
+
+    Returns:
+        [List of list of integers]: List of neighbors with each neighbor containing a list of their row and column index.
+    """
     neighbors = []
     for x in range(-1, 2, 1):
         for y in range(-1, 2, 1):
@@ -58,6 +107,15 @@ def get_neighbors(r_index, c_index):
 
 
 def generate_board(r_size, c_size):
+    """Generate board of row size, column size
+
+    Args:
+        r_size (int): Number of rows to generate
+        c_size (int): Number of columns to generate
+
+    Returns:
+        [List of lists of string]: Board that can be used to play Conway's GOL
+    """
     board = []
     for x in range(0, r_size):
         row = []
@@ -68,6 +126,11 @@ def generate_board(r_size, c_size):
 
 
 def pretty_print(board):
+    """Pretty print a board
+
+    Args:
+        board (List of lists of string): Board to be printed!
+    """
     for row in board:
         for char in row:
             print(*char, end='')
@@ -75,6 +138,14 @@ def pretty_print(board):
 
 
 def random_alive(board):
+    """Randomly make a subset of the board ALIVE!
+
+    Args:
+        board (List of lists of string): Board to modify
+
+    Returns:
+        [List of lists of string]: Modified board
+    """
     r_index = -1
     for rows in board:
         c_index = -1
@@ -87,10 +158,25 @@ def random_alive(board):
 
 
 def random_number(n):
-    return random.randint(0, 1)
+    """Generate a random number from 0 to n
+
+    Args:
+        n (int): Limit of number to be generated (inclusive)
+
+    Returns:
+        [int]: Random generated number
+    """
+    return rand.randint(0, n)
 
 
 def run_game(row, column, times):
+    """Run the game a certain number of times for a board of size row, column,
+
+    Args:
+        row (int): Number of rows on the board
+        column (int): Number of columns on the board
+        times (int): Number of times to be run
+    """
     board = generate_board(row, column)
     board = random_alive(board)
     pretty_print(board)
@@ -101,6 +187,12 @@ def run_game(row, column, times):
 
 
 def custom_game(board, times):
+    """Run the game on your custom board
+
+    Args:
+        board (List of lists of lists of lists of strings): Board to run the game on
+        times (int): How many times to run the game
+    """
     pretty_print(board)
     for time in range(0, times):
         print("Iteration: " + str(time))
@@ -109,11 +201,19 @@ def custom_game(board, times):
 
 
 def clear():
-    # Nasty!
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """Clear the terminal
+    """
+    os.system('cls' if os.name ==
+              'nt' else 'clear')  # Nasty way to do this, find alternative
 
 
 def stream_game(row, column):
+    """Play the game an infinite number of times for a row, column
+
+    Args:
+        row (int): Number of rows on the board
+        column (int): Number of columns on the board
+    """
     # Depending on game and size, can cause flashing, find other way to stream
     clear()
     board = generate_board(row, column)
@@ -126,6 +226,11 @@ def stream_game(row, column):
 
 
 def main(argv):
+    """Parse command line arguments and run the game accordingly
+
+    Args:
+        argv (Command line arguments): Passed command line arguments
+    """
     # This is janky, better ways to do this!
     try:
         opts, args = getopt.getopt(argv, "hi:r:c:", ["rows=", "columns="])
@@ -151,7 +256,7 @@ def main(argv):
             column = arg
     try:
         stream_game(int(row), int(column))
-    except Exception as e:
+    except Exception:  # TODO: Handle specific exception
         print('Error! Invalid row/column, please use:')
         print('main.py -h')
         sys.exit()
